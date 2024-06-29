7c30bcd103850ab9b5f1b210b5824eb21a373a49
JAFB537 <jafb537@gmail.com>
2024-06-29 12:08:10 -0600

    Update Drop Data Base

--- DropDataBase.sql
+++ DropDataBase.sql
@@ -1,9 +1,10 @@
 --------------------------------------- CREATE DATABASE ---------------------------------------
+USE master;
+GO
 
 -- Delete DataBase If Exist
 IF EXISTS (SELECT * FROM sys.databases WHERE name = 'ToDoDB')
 BEGIN
-    USE master;
     ALTER DATABASE ToDoDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE ToDoDB;
 END
