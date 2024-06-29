9f91529a37b5938208480c4fd895f28746a85002
JAFB537 <jafb537@gmail.com>
2024-06-29 12:08:44 -0600

    Update Users

--- Users.sql
+++ Users.sql
@@ -4,10 +4,24 @@ GO
 
 ---------------------------------------- Create User Data Base ---------------------------------------
 
-USE ToDoDB;
-CREATE LOGIN ToDoUser 
-   WITH PASSWORD = 'Admin1234+';
-CREATE USER ToDoUser 
-   FOR LOGIN ToDoUser;
+---> Create a login and user for ToDoUser (if not already created)
 
+IF NOT EXISTS (SELECT name FROM sys.server_principals WHERE name = 'ToDoUser')
+BEGIN
+    CREATE LOGIN ToDoUser WITH PASSWORD = 'Admin1234+';
+END
+
+IF NOT EXISTS (SELECT name FROM sys.database_principals WHERE name = 'ToDoUser')
+BEGIN
+    CREATE USER ToDoUser FOR LOGIN ToDoUser;
+END
+
+---> Grant CONTROL permissions on the database to ToDoUser
 GRANT CONTROL ON DATABASE::ToDoDB TO ToDoUser;
+
+---------------------------------------- User Roles ---------------------------------------
+
+---> Grant SELECT permission on specific schemas/tables/views to ToDoUser
+
+GRANT SELECT ON SCHEMA::dbo TO ToDoUser; 
+
